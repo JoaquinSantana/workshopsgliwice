f6361f93bef098ce553e843b2ca4c56db397b354
Tomasz Panek <joachimsantana@gmail.com>
2015-05-07 10:31:20 +0200

    Add user fields to permitted attributes during update account action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { 
       |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation)
+    }    
+    devise_parameter_sanitizer.for(:account_update) { 
+      |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation)
     }
   end
 end
