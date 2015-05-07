43c3eaa52d4ebaf7bb2c37dad9ee3f897029dc6f
Tomasz Panek <joachimsantana@gmail.com>
2015-05-07 10:48:54 +0200

    Validates correct format of price and write method #average_rating for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,10 @@ class Product < ActiveRecord::Base
   has_many :reviews
 
   validates :title, :description, :price, presence: true
+  validates_format_of :price, :with => /\A\d+(?:\.\d{0,2})?\z/
+
+  def average_rating
+    reviews.pluck(:rating).sum / reviews.size.to_f
+  end
+  
 end
